e7663d901f86abfe2f1038a836f53391a0eed73b
Dante-B216 <2916307326@qq.com>
2025-03-08 02:19:36 +0800

    分割页面+cos删除/批量删除文件

--- utils/tencent/cos.py
+++ utils/tencent/cos.py
@@ -7,6 +7,7 @@ import logging
 from django.conf import settings
 
 
+# 创建桶
 def create_bucket(bucket, region="ap-guangzhou"):
     """
     创建桶
@@ -48,6 +49,7 @@ def create_bucket(bucket, region="ap-guangzhou"):
     )
 
 
+# 上传文件到桶
 def upload_file(bucket, region, file_object, key):
     # 把图片对象上传到当前用户的桶
     secret_id = settings.COS_SECRET_ID
@@ -65,3 +67,67 @@ def upload_file(bucket, region, file_object, key):
     )
 
     return "https://{}.cos.{}.myqcloud.com/{}".format(bucket, region, key)
+
+
+# 桶删除文件
+def delete_file(bucket, region, key):
+    secret_id = settings.COS_SECRET_ID
+
+    secret_key = settings.COS_SECRET_KEY
+
+    config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key)
+
+    client = CosS3Client(config)
+
+    client.delete_object(
+        Bucket=bucket,
+        Key=key,  # 文件名
+    )
+
+# 桶批量删除文件
+def delete_file_list(bucket, region, key_list):
+    """
+
+    :param bucket:
+    :param region:
+
+    :param key_list:传入的数据格式如下
+    [
+        {"Key": "file_name1"},
+        {"Key": "file_name2"}
+    ]
+
+    :return:
+    """
+    secret_id = settings.COS_SECRET_ID
+
+    secret_key = settings.COS_SECRET_KEY
+
+    config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key)
+
+    client = CosS3Client(config)
+
+    """
+    批量删除文件
+    objects = {
+        "Quiet": "true",
+        "Object": [
+            {
+                "Key": "file_name1"
+            },
+            {
+                "Key": "file_name2"
+            }
+        ]
+    }
+    """
+
+    objects = {
+        "Quiet": "true",
+        "Object": key_list
+    }
+
+    client.delete_objects(
+        Bucket=bucket,
+        Delete=objects,  # 文件列表
+    )
